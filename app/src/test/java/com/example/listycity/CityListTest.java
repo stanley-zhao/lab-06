362b06695ecb482a4a2486f047f8a6938cd8efd2
stanley-zhao <stanleyzhao.ca@outlook.com>
2026-02-12 17:53:39 -0700

    test

--- app/src/test/java/com/example/listycity/CityListTest.java
+++ app/src/test/java/com/example/listycity/CityListTest.java
@@ -10,4 +10,14 @@ class CityListTest {
     private City mockCity() {
         return new City("Edmonton", "Alberta");
     }
+
+    @Test
+    void testAdd() {
+        CityList cityList = mockCityList();
+        assertEquals(1, cityList.getCities().size());
+        City city = new City("Regina", "Saskatchewan");
+        cityList.add(city);
+        assertEquals(2, cityList.getCities().size());
+        assertTrue(cityList.getCities().contains(city));
+    }
 }
