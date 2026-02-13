060c0eb714d580f915273491b13582694b1d97b2
stanley-zhao <stanleyzhao.ca@outlook.com>
2026-02-12 18:05:59 -0700

    finish lab demo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,5 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:6.0.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:6.0.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.0.2")
+    implementation(files("/Users/yongestreet/Library/Android/sdk/platforms/android-36/android.jar"))
 }
