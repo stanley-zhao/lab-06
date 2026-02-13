7be1013aaa51bccf7c9c320f8413d24b17b1eb54
stanley-zhao <stanleyzhao.ca@outlook.com>
2026-02-12 17:48:39 -0700

    test

--- app/src/main/java/com/example/listycity/CityList.java
+++ app/src/main/java/com/example/listycity/CityList.java
@@ -1,15 +1,12 @@
 package com.example.listycity;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 /**
- * This is a class that keeps a list of city objects
+ * This is a class that keeps track of a list of city objects
  */
 public class CityList {
     private List<City> cities = new ArrayList<>();
-
     /**
      * This adds a city to the list if the city does not exist
      * @param city
@@ -21,7 +18,6 @@ public class CityList {
         }
         cities.add(city);
     }
-
     /**
      * This returns a sorted list of cities
      * @return
---
+++ app/src/test/java/com/example/listycity/CityListTest.java
@@ -0,0 +1,13 @@
+package com.example.listycity;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+class CityListTest {
+    private CityList mockCityList() {
+        CityList cityList = new CityList();
+        cityList.add(mockCity());
+        return cityList;
+    }
+    private City mockCity() {
+        return new City("Edmonton", "Alberta");
+    }
+}
